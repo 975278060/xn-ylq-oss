907ceedba36a84f6fcdfb337478e30a8a05a3a77
A8GX0ETIUCTXSGF\Administrator <www.975278060@qq.com>
2018-11-22 22:25:48 +0800

    按钮不显示

---
+++ src/reducer.js
@@ -0,0 +1,79 @@
+import { combineReducers } from 'redux';
+
+import { user } from './redux/user';
+import { menu } from './redux/menu';
+import { securityRole } from './redux/security/role';
+import { securityMenu } from './redux/security/menu';
+import { securitySysParam } from './redux/security/sysParam';
+import { securityUser } from './redux/security/user';
+import { securityDataDict } from './redux/security/dataDict';
+import { publicBanner } from './redux/public/banner';
+import { publicNotice } from './redux/public/notice';
+/** ***** 财务管理start ***** **/
+// 财务管理--平台账户
+import { platformDistributionRules } from './redux/finance/platform/distributionRules';
+import { platformGradationRules } from './redux/finance/platform/gradationRules';
+import { platformIntegralRules } from './redux/finance/platform/integralRules';
+import { platformPayRules } from './redux/finance/platform/payRules';
+import { platformMemberLevel } from './redux/finance/platform/memberLevel';
+import { platformTppRules } from './redux/finance/platform/tppRules';
+import { platformInvitation } from './redux/finance/platform/invitation';
+import { platformAccount } from './redux/finance/platform/account';
+import { platformAccountFlows } from './redux/finance/platform/accountFlows';
+import { platformAccountList } from './redux/finance/platform/accountList';
+import { platformFlows } from './redux/finance/platform/flows';
+// 财务管理--充值管理
+import { rechargeRecharges } from './redux/finance/recharge/recharges';
+import { rechargeRecords } from './redux/finance/recharge/records';
+// 财务管理--取现管理
+import { withdrawRules } from './redux/finance/withdraw/rules';
+import { withdrawWithdraw } from './redux/finance/withdraw/withdraw';
+import { withdrawRecords } from './redux/finance/withdraw/records';
+// 业务管理--用户管理
+import { userUsers } from './redux/biz/user/users';
+import { userUserAccounts } from './redux/biz/user/user-accounts';
+import { userSignIn } from './redux/biz/user/signIn';
+import { userShares } from './redux/biz/user/shares';
+import { userGives } from './redux/biz/user/gives';
+import { userAccounts } from './redux/biz/user/accounts';
+import { userAccountFlows } from './redux/biz/user/account-flows';
+import { userFlows } from './redux/biz/user/flows';
+// 业务管理--业务规则
+import { rulesWeight } from './redux/biz/rules/weight';
+
+export default combineReducers({
+  user,
+  menu,
+  securityRole,
+  securityMenu,
+  securityUser,
+  securitySysParam,
+  securityDataDict,
+  publicBanner,
+  publicNotice,
+  platformDistributionRules,
+  platformGradationRules,
+  platformIntegralRules,
+  platformPayRules,
+  platformMemberLevel,
+  platformTppRules,
+  platformInvitation,
+  platformAccount,
+  platformAccountFlows,
+  platformAccountList,
+  platformFlows,
+  rechargeRecharges,
+  rechargeRecords,
+  withdrawRules,
+  withdrawWithdraw,
+  withdrawRecords,
+  userUsers,
+  userUserAccounts,
+  userSignIn,
+  userShares,
+  userGives,
+  userAccounts,
+  userAccountFlows,
+  userFlows,
+  rulesWeight
+});
